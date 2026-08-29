45789ad5aa4305e3a1d8830dc9310b5ac7c45aea
sheiman125 <sheiman1211@gmail.com>
2026-08-29 16:13:57 +0800

    Test automatic deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! My first DevOps CI/CD project is running."
+    return "MY NAME IS SIMON"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
